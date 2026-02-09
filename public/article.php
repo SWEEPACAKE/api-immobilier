7af34bb9e94d0cc4479e5685ac1168af9efa5262
SWEEPACAKE <nicolas.savoca.info@gmail.com>
2026-02-09 10:10:52 +0100

    Avec JWT Handler

--- public/article.php
+++ public/article.php
@@ -1,7 +1,7 @@
 <?php
 $config = require_once __DIR__ . '/includes/config.php';
 
-// include 'includes/jwt-handler.php';
+include 'includes/jwt-handler.php';
 include 'includes/db.php';
 
 if(empty($_GET)) {
--- public/types.php
+++ public/types.php
@@ -1,5 +1,6 @@
 <?php
 $config = require_once __DIR__ . '/includes/config.php';
+include 'includes/jwt-handler.php';
 include 'includes/db.php';
 
 $query = "SELECT type.* FROM type";
