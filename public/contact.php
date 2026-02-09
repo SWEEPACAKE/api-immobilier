777f42ccd0b9a67d6baf5fd7f83de39af36a079f
SWEEPACAKE <nicolas.savoca.info@gmail.com>
2026-02-09 14:29:59 +0100

    Contact mais avec la vérification de Token

--- public/contact.php
+++ public/contact.php
@@ -1,6 +1,6 @@
 <?php
 $config = require_once __DIR__ . '/includes/config.php';
-// include 'includes/jwt-handler.php';
+include 'includes/jwt-handler.php';
 include 'includes/db.php';
 
 if(!empty($_POST)) {
